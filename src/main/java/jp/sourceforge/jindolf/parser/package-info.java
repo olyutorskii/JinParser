8374c35cdd7c03da0b25b86e14b97d38bafd7ff2
Olyutorskii <olyutorskii@users.osdn.me>
2016-06-15 22:34:41 +0900

    OSDN.JP対応

--- src/main/java/jp/sourceforge/jindolf/parser/package-info.java
+++ src/main/java/jp/sourceforge/jindolf/parser/package-info.java
@@ -102,9 +102,9 @@
  *
  * <hr>
  *
- * @see <a href="http://jindolf.sourceforge.jp/">
+ * @see <a href="http://jindolf.osdn.jp/">
  * Jindolfポータルサイト</a>
- * @see <a href="http://sourceforge.jp/projects/jindolf/">
+ * @see <a href="https://osdn.jp/projects/jindolf/devel/">
  * Jindolf開発プロジェクト</a>
  */
 
